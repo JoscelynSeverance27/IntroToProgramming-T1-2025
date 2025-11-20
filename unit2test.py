1dc54eadebc6d6850c62d2d03aad053956d9a1c6
JoscelynSeverance27 <829143@mystma.org>
2025-11-20 16:07:35 -0600

    Add files via upload

---
+++ unit2test.py
@@ -0,0 +1,30 @@
+a = input("Enter a word: ")
+
+b = input("Enter another word: ")
+
+c = input("Enter a third word: ")
+
+print(a, b, c)
+
+one = input("Enter an integer: ")
+
+two = input("Enter another integer: ")
+
+three = input("Enter a third integer: ")
+
+def add_three():
+    print(int(one) + int(two) + int(three))
+
+add_three()
+
+d = input("Enter a word: ")
+at = input("Enter an integer: ")
+t = input("Enter a float: ")
+
+#this one doesnt work but i did my best vv
+def data_three():
+
+    x = (int(at) + float(t))
+    print(x + d)
+
+data_three()
