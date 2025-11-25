2f6a15ff7ec30cacd637f50ba04ecbfe8cf761c2
JoscelynSeverance27 <829143@mystma.org>
2025-11-25 13:06:57 -0600

    Add files via upload

---
+++ animal_survey.py
@@ -0,0 +1,6 @@
+animal = input("What is your favorite animal?: ")
+food = input("What do they eat?: ")
+why = input("Why do you like them?: ")
+where = input("Where do they live?: ")
+
+print("Your favorite animal is the " + animal + ". They eat " + food + " and live in the " + where + ". You like them because " + why + ".")
